606908865e1282ac8abe3cfe0bf128433dd9e2f3
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2018-12-12 13:05:36 -0500

    Added Files
    Day 1 Completed Work

--- PineTree.java
+++ PineTree.java
@@ -3,5 +3,10 @@ import java.applet.*;
 import java.util.*;
 public class PineTree extends Tree
 {
-   
+   private Trunk trunk;
+   private PineLeaves leaves;
+   public PineTree(int x1, int y1){
+       super(x1, y1);
+       
+    }
 }
