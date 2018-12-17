bf5938a095b99ccce537903137811daba4296bcd
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2018-12-17 16:24:49 -0500

    Update AppletRunner.java

--- Day 3/AppletRunner.java
+++ Day 3/AppletRunner.java
@@ -5,9 +5,16 @@ public class AppletRunner extends Applet
 {
     public void paint(Graphics g) {
         this.setSize(1600, 900);
+        g.fillRect(0, 0, 1600, 900);
         Tree tree = new Tree(100, 100);
         tree.draw(g);
         PineTree tree2 = new PineTree(500, 150); 
         tree2.draw(g);
+        ChristmasTree tree3 = new ChristmasTree(500, 500, 500, 550);
+        tree3.draw(g);
+        ChristmasTree tree4 = new ChristmasTree(300, 300, 300, 350);
+        tree4.draw(g);
+        Box box = new Box(1000, 250, 50, 50);
+        box.draw(g);
     }
 }
