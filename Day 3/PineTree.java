10d718e95905aef754938a9be33d58d6cd61eae8
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2018-12-17 10:58:56 -0500

    Update PineTree.java

--- Day 3/PineTree.java
+++ Day 3/PineTree.java
@@ -1,25 +1,19 @@
+
+
 import java.awt.*;
 import java.applet.*;
 import java.util.*;
 public class PineTree extends Tree
 {
-   protected Point p1;
-   protected Point p2;
-    public PineTree(int x1, int y1) {
+   private Trunk trunk;
+   private PineLeaves leaves;
+   public PineTree(int x1, int y1) {
        super(x1, y1);
-       Point p1 = new Point(x1, y1);
-       Point p2 = new Point(x1, y1);
+       leaves = new PineLeaves(x1, y1, 100, 100);
+       trunk = new Trunk(x1, y1, 25, 100);
    }
    public void draw(Graphics g) {
-        drawLeaves(g);
+        leaves.draw(g);
         trunk.draw(g);
-   }
-   public void drawLeaves(Graphics g) {
-        g.setColor(new Color(97, 153, 59));
-        Polygon leaves = new Polygon();
-        leaves.addPoint(p1.getX() + 50, p1.getY());
-        leaves.addPoint(p1.getX(), p1.getY() + 50);
-        leaves.addPoint(p2.getX(), p2.getY() + 50);
-        g.fillPolygon(leaves);
     }
 }
