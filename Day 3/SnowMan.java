32dabd8acfc22ee00737a979763d0b63593239fc
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2018-12-20 09:53:40 -0500

    Update SnowMan.java

--- Day 3/SnowMan.java
+++ Day 3/SnowMan.java
@@ -3,5 +3,14 @@ import java.applet.*;
 import java.util.*;
 public class SnowMan extends Man
 {
-    
+    public SnowMan(int x1, int y1, int x2, int y2) {
+        super( x1 , y1 , x2 , y2 );
+    }
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillOval(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+        
+        g.fillOval(p1.getX() - p2.getY() / 2, p1.getY() + p2.getY() , p2.getX() * 2, p2.getY() * 2);
+        g.fillOval(p1.getX() - p2.getY(), p1.getY() + p2.getY() * 3 , p2.getX() * 3, p2.getY() * 3);
+    }
 }
