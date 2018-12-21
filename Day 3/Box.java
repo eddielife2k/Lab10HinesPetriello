2c593b1f0cdee2588adca01b961f1f91c0ae3c64
eddielife2k <44607352+eddielife2k@users.noreply.github.com>
2018-12-21 12:02:46 -0500

    Update Box.java

--- Day 3/Box.java
+++ Day 3/Box.java
@@ -13,11 +13,26 @@ public class Box
         g.setColor(new Color(80, 42, 42));
         g.fillRect(p1.getX(), p1.getY(), p2.getX(), p2.getX());
         g.fillRect(p1.getX()+50,p1.getY()+50,p2.getX(),p2.getX());
-        g.drawLine(p1.getX(), p1.getY(),p1.getX()+50,p1.getY()+50);
-        g.drawLine(p1.getX(), p1.getY()+p2.getX(), p1.getX()+50,p1.getY()+50+p2.getX());
-        g.drawLine(p1.getX()+p2.getX(),p1.getY(), p1.getX()+50+ p2.getX(),p1.getY()+50);
-        g.drawLine(p1.getX()+p2.getX(),p1.getY()+p2.getX(),
-        p1.getX()+ 50 + p2.getX(), p1.getY() + 50 + p2.getX());
+        
+        Polygon boxSide = new Polygon();
+        boxSide.addPoint( p1.getX(), p1.getY());
+        boxSide.addPoint( p1.getX(), p1.getY() + 50 );
+        boxSide.addPoint( p1.getX() + 50, p1.getY() + 100 );
+        boxSide.addPoint( p1.getX() + 50, p1.getY() + 50  );
+        g.fillPolygon(boxSide);
+        
+        Polygon boxIn  = new Polygon();
+        boxIn.addPoint( p1.getX() + 50, p1.getY() );
+        boxIn.addPoint( p1.getX() + 50, p1.getY() + 50);
+        boxIn.addPoint( p1.getX() + 100, p1.getY() + 50);
+        g.setColor(new Color(40, 24, 24));
+        g.fillPolygon(boxIn);
+        
+        Polygon boxB   = new Polygon();
+        boxB.addPoint( p1.getX(), p1.getY() );
+        boxB.addPoint( p1.getX() + 50, p1.getY() );
+        boxB.addPoint( p1.getX() + 50, p1.getY() + 50);
+        g.fillPolygon(boxB); 
       
     }
 }
